52c4ec550b7d84a4229a02f670c90ddabe9859ef
RR Web Designer <rr@rogerrobles.com>
2019-03-23 13:46:04 -0400

    Create header.php
    Created static  header for every page.

---
+++ includes/header.php
@@ -0,0 +1,28 @@
+<?php
+	$companyName = "Chef Latina's Fine Dining";
+	include('includes/arrays.php');
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title><?php echo TITLE; ?></title>
+	<link href="includes/assets/styles.css" rel="stylesheet">
+</head>
+
+<body id="final-example">
+
+	<div class="wrapper">
+
+		<div id="banner">
+			<a href="/" title="Return to Home">
+				<img src="includes/assets/img/banner.png" alt="Chef Latina's Fine Dining">
+			</a>
+		</div><!-- banner -->
+
+		<div id="nav">
+			<?php include('includes/nav.php'); ?>
+		</div><!-- nav -->
+
+		<div class="content">
